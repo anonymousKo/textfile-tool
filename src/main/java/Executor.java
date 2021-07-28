60eb5f4dd0de87885257876d2303d5d1ae84cbde
koresuki <1789151097@qq.com>
2021-07-28 17:30:16 +0800

    modify the identification of param

--- src/main/java/Executor.java
+++ src/main/java/Executor.java
@@ -1,27 +1,34 @@
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Scanner;
+
 @Slf4j
 public class Executor {
     public static void main(String[] args) {
         String path = System.getProperty("user.dir");
         String filePath = path.substring(0,path.lastIndexOf("\\"));
         try {
-            if (args == null || args.length <1){
+            if (args.length != 1){
                 throw new Exception("No input param");
             }
             if (Integer.parseInt(args[0]) != 2 && Integer.parseInt(args[0]) != 1) {
                 log.info("InvalidOperationNum !");
             } else if (Integer.parseInt(args[0]) == 1) {
                 log.info("Current path :{}", filePath);
-                if (args.length != 3) {
-                    throw new Exception("Cannot resolve replacement");
-                } else {
-                    String oldStr = args[1];
-                    String newStr = args[2];
-                    FileOperationUtil fileOperationUtil = new FileOperationUtil();
-                    fileOperationUtil.replace(filePath, oldStr, newStr);
+                Scanner s = new Scanner(System.in);
+                String oldStr = null;
+                String newStr = "";
+                System.out.print("input the oldStr: ");
+                if(s.hasNext()){
+                    oldStr = s.nextLine();
                 }
+                System.out.print("input the newStr: ");
+                if(s.hasNextLine()){
+                    newStr = s.nextLine();
+                }
+                FileOperationUtil fileOperationUtil = new FileOperationUtil();
+                fileOperationUtil.replace(filePath, oldStr, newStr);
             } else if (Integer.parseInt(args[0]) == 2) {
                 log.info("Current path :{}", filePath);
                 FileOperationUtil fileOperationUtil = new FileOperationUtil();
